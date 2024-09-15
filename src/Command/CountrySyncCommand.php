c051fa4530fc4f1d49d2a33ec20320460ae5dd48
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2024-09-15 21:07:50 +0300

    implement CountrySyncCommand

--- src/Command/CountrySyncCommand.php
+++ src/Command/CountrySyncCommand.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use App\Service\CountrySyncService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class CountrySyncCommand extends Command
 {
+    public function __construct(
+        private CountrySyncService $countrySyncService,
+    ) {
+        parent::__construct();
+    }
+
     protected function configure(): void
     {
         $this->setName('countries:sync');
@@ -18,6 +26,17 @@ class CountrySyncCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $io = new SymfonyStyle($input, $output);
+
+        try {
+            $this->countrySyncService->syncCountries();
+            $io->success('Countries have been successfully synced.');
+        } catch (\Exception $e) {
+            $io->error('Error syncing countries: '.$e->getMessage());
+
+            return Command::FAILURE;
+        }
+
         return Command::SUCCESS;
     }
 }
